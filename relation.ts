947efe09e6471b45947c7980a5eacdb339100d05
priyam karn <83950448+priyamkarn@users.noreply.github.com>
2024-08-22 21:03:17 +0530

    Create relation.ts

---
+++ relation.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function getTodosAndUserDetails(userId: number) {
+    try {
+        const todos = await prisma.todo.findMany({
+            where: {
+                userId: userId,
+            },
+            select: {
+                title: true,
+                description: true,
+                user: {
+                    select: {
+                        username: true // Include only the username of the user
+                    }
+                }
+            }
+        });
+        console.log(todos);
+    } catch (error) {
+        console.error("Error fetching todos:", error);
+    } finally {
+        await prisma.$disconnect();
+    }
+}
+getTodosAndUserDetails(1);
